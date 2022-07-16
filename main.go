db348519591ec6aebb88094b2786859f545f5208
tvhir <tahirsaif100@gmail.com>
2022-07-16 15:51:36 -0400

    moved extraction into ETL folder

--- main.go
+++ main.go
@@ -2,75 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"time"
 
-	"github.com/gocarina/gocsv"
+	ETL "github.com/tvhir/TradovateManager/ETL"
 )
-
-type Trade struct {
-	Symbol          string             `csv:"symbol"`
-	TickSize        float32            `csv:"_tickSize"`
-	Qty             int                `csv:"qty"`
-	BuyPrice        float32            `csv:"buyPrice"`
-	SellPrice       float32            `csv:"sellPrice"`
-	PnL             string             `csv:"pnl"`
-	BoughtTimestamp TradovateTimestamp `csv:"boughtTimestamp"`
-	SoldTimestamp   TradovateTimestamp `csv:"soldTimestamp"`
-	Duration        TradovateDuration  `csv:"duration"`
-}
-
-type TradovateTimestamp struct {
-	time.Time
-}
-
-// Convert the CSV string as internal date
-func (date *TradovateTimestamp) UnmarshalCSV(timestampString string) (err error) {
-	date.Time, err = time.Parse("01/02/2006 15:04:05", timestampString)
-	return err
-}
-
-type TradovateDuration struct {
-	time.Duration
-}
-
-// Convert the CSV string as internal date
-func (duration *TradovateDuration) UnmarshalCSV(durationString string) (err error) {
-	durationArray := strings.Fields(durationString)
-	for i, duration := range durationArray {
-		if strings.Contains(duration, "min") {
-			durationArray[i] = strings.Replace(duration, "min", "m", 1)
-		}
-		if strings.Contains(duration, "sec") {
-			durationArray[i] = strings.Replace(duration, "sec", "s", 1)
-		}
-	}
-	duration.Duration, err = time.ParseDuration(strings.Join(durationArray, ""))
-	if err != nil {
-		panic(err)
-	}
-	return err
-}
 
 func main() {
 	fmt.Println("Tradovate Manager")
 
-	//Open CSV
-	performanceCSV, err := os.Open("Performance.07.11-15.22.csv")
-	if err != nil {
-		fmt.Println(err)
-	}
+	//Extract data from CSV, recieve struct with data
+	trades := ETL.Extract()
 
-	fmt.Println("Opened CSV")
-	defer performanceCSV.Close()
-
-	// read and print CSV file
-	trades := []*Trade{}
-
-	if err := gocsv.UnmarshalFile(performanceCSV, &trades); err != nil {
-		panic(err)
-	}
 	for _, trade := range trades {
 		fmt.Printf("%+v\n", *trade)
 	}
